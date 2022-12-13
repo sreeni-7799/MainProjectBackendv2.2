e6a800093ec5369b34551ac348ffa92408cf1bcf
Vivin V Abraham <vivin.abraham@experionglobal.com>
2022-12-13 18:35:56 +0530

    nominee_post

---
+++ src/main/java/com/experion/mainbackend/dto/NomineePost.java
@@ -0,0 +1,18 @@
+package com.experion.mainbackend.dto;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class NomineePost {
+    private Long nomineeId;
+    private Long chittalId;
+    private String name;
+    private Integer age;
+    private Date dob;
+    private Long phone;
+    private String address;
+    private Long pincode;
+    private Long aadhar;
+}
